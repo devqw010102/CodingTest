918065a0e65812504a9e76cd168ed93569e2d98a
devqw010102 <qw010102@gmail.com>
2025-10-21 17:01:25 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.07 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        String str = String.valueOf(n);
+        
+        for(int i = 0; i < str.length(); i++) {
+            answer += Integer.parseInt(String.valueOf(str.charAt(i)));
+        }
+        
+        
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        // System.out.println("Hello Java");
+
+        return answer;
+    }
+}
